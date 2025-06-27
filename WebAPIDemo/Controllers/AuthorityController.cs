f015bca89420c3ebdd1ed5b231fdbf1372bc1584
promityadav <promit.yadav@gmail.com>
2025-06-27 13:51:33 +0530

    Azure

--- WebAPIDemo/Controllers/AuthorityController.cs
+++ WebAPIDemo/Controllers/AuthorityController.cs
@@ -22,46 +22,46 @@ namespace WebAPIDemo.Controllers
         public IActionResult Authenticate([FromBody] AppCredential credential)
         {
 
-            var Application = db.Applications.FirstOrDefault(u => u.ClientId == credential.ClientId && u.Secret == credential.Secret);
-            if (Application == null)
-            {
-                ModelState.AddModelError("Unauthorized", "You are not authorised.");
-                var problemDetails = new ValidationProblemDetails(ModelState)
-                {
-                    Status = StatusCodes.Status401Unauthorized
-                };
-                return new UnauthorizedObjectResult(problemDetails);
-            }
-            else
-            {
-                
-                var expierAt = DateTime.UtcNow.AddMinutes(10);
-                return Ok(new
-                {
-                    access_token = Authenticator.CreateToken(credential.ClientId, expierAt, configuration["SecurityKey"] ?? String.Empty),
-                    expires_at = expierAt
-                });
-            }
-
-            //if (Authenticator.Authenticate(credential.ClientId, credential.Secret))
+            //var Application = db.Applications.FirstOrDefault(u => u.ClientId == credential.ClientId && u.Secret == credential.Secret);
+            //if (Application == null)
             //{
-            //    var expierAt = DateTime.UtcNow.AddMinutes(1);
+            //    ModelState.AddModelError("Unauthorized", "You are not authorised.");
+            //    var problemDetails = new ValidationProblemDetails(ModelState)
+            //    {
+            //        Status = StatusCodes.Status401Unauthorized
+            //    };
+            //    return new UnauthorizedObjectResult(problemDetails);
+            //}
+            //else
+            //{
+
+            //    var expierAt = DateTime.UtcNow.AddMinutes(10);
             //    return Ok(new
             //    {
             //        access_token = Authenticator.CreateToken(credential.ClientId, expierAt, configuration["SecurityKey"] ?? String.Empty),
             //        expires_at = expierAt
             //    });
             //}
-            //else
-            //{
-            //    ModelState.AddModelError("Unauthorized", "You are not authorised.");
-            //    var problemDetails = new ValidationProblemDetails(ModelState)
-            //    {
-            //        Status = StatusCodes.Status401Unauthorized
 
-            //    };
-            //    return new UnauthorizedObjectResult(problemDetails);
-            //}
+            if (Authenticator.Authenticate(credential.ClientId, credential.Secret))
+            {
+                var expierAt = DateTime.UtcNow.AddMinutes(1);
+                return Ok(new
+                {
+                    access_token = Authenticator.CreateToken(credential.ClientId, expierAt, configuration["SecurityKey"] ?? String.Empty),
+                    expires_at = expierAt
+                });
+            }
+            else
+            {
+                ModelState.AddModelError("Unauthorized", "You are not authorised.");
+                var problemDetails = new ValidationProblemDetails(ModelState)
+                {
+                    Status = StatusCodes.Status401Unauthorized
+
+                };
+                return new UnauthorizedObjectResult(problemDetails);
+            }
         }
 
 
